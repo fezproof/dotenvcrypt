562b1dcfbeace848e06ba4114965e8b38217c84c
fezproof <stevebob1997@hotmail.com>
2018-12-17 00:37:25 +0800

    updated code to use filestreams

--- index.js
+++ index.js
@@ -6,34 +6,57 @@ const IV_LENGTH = 16; // For AES, this is always 16
 const ENCRYPTED_FILE = '.env.enc';
 const DECRYPTED_FILE = '.env';
 
-function encrypt(password) {
-  const text = fs.readFileSync(DECRYPTED_FILE);
+const { Transform } = require('stream');
 
+class AppendInitVect extends Transform {
+  constructor(initVect, opts) {
+    super(opts);
+    this.initVect = initVect;
+    this.appended = false;
+  }
+
+  _transform(chunk, encoding, cb) {
+    if (!this.appended) {
+      this.push(this.initVect);
+      this.appended = true;
+    }
+    this.push(chunk);
+    cb();
+  }
+}
+
+function encrypt(password) {
   const iv = crypto.randomBytes(IV_LENGTH);
 
   const key = crypto.scryptSync(password, 'salt', 32);
 
   const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
 
-  let encrypted = cipher.update(text);
+  const readStream = fs.createReadStream(DECRYPTED_FILE);
+  const appendIV = new AppendInitVect(iv);
+  const writeSteam = fs.createWriteStream(ENCRYPTED_FILE);
 
-  encrypted = Buffer.concat([encrypted, cipher.final()]);
-  fs.writeFileSync(ENCRYPTED_FILE, `${iv.toString('hex')}:${encrypted.toString('hex')}`);
+  readStream.pipe(cipher).pipe(appendIV).pipe(writeSteam);
 }
 
 function decrypt(password) {
-  const text = fs.readFileSync(ENCRYPTED_FILE).toString();
+  const readInitVect = fs.createReadStream(ENCRYPTED_FILE, { end: 15 });
 
-  const textParts = text.split(':');
-  const iv = Buffer.from(textParts.shift(), 'hex');
-  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  const key = crypto.scryptSync(password, 'salt', 32);
+  let iv;
+  readInitVect.on('data', (chunk) => {
+    iv = chunk;
+  });
 
-  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-  let decrypted = decipher.update(encryptedText);
+  readInitVect.on('close', () => {
+    const key = crypto.scryptSync(password, 'salt', 32);
+    const readStream = fs.createReadStream(ENCRYPTED_FILE, { start: 16 });
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    const writeStream = fs.createWriteStream(DECRYPTED_FILE);
 
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-  fs.writeFileSync(DECRYPTED_FILE, decrypted.toString());
+    readStream
+      .pipe(decipher)
+      .pipe(writeStream);
+  });
 }
 
 module.exports = { decrypt, encrypt };
